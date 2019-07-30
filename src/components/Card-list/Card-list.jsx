a06e2853187640ba789fe4b6f2ec99c7bb29d02b
Jablan <joshuaablan@gmail.com>
2019-07-30 14:37:21 -0700

    add separate Card component

--- src/components/Card-list/Card-list.jsx
+++ src/components/Card-list/Card-list.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Card from "../Card/Card"
+
 import "./card-list.styles.css"
 
 const CardList = props => 
         <div className="card-list">
             {
                 props.monsters.map((monster,i) =>
-                    <h1 key={i}>{monster.name}</h1>
+                    <Card key={i} monster={monster}/>
                 )
             }
         </div>
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = props => 
+    <div>   
+        <h1>
+            {props.monster.name}
+        </h1>
+    </div>
+
+
+
+export default Card
